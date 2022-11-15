2ead5f31565480031fc8fa4b6046cf9a9971a991
maratorium <73485465+maratoriy@users.noreply.github.com>
2022-11-16 00:58:33 +0300

    Initial commit

---
+++ ast.c
@@ -0,0 +1,158 @@
+/* ast.c */
+
+#include <stdlib.h>
+
+#include "ast.h"
+
+struct AST *newnode(struct AST ast) {
+    struct AST *const node = malloc(sizeof(struct AST));
+    *node = ast;
+    return node;
+}
+
+struct AST _lit(int64_t value) {
+    return (struct AST) {AST_LIT, .as_literal = {value}};
+}
+
+struct AST *lit(int64_t value) {
+    return newnode(_lit(value));
+}
+
+struct AST _unop(enum unop_type type, struct AST *operand) {
+    return (struct AST) {AST_UNOP, .as_unop = {type, operand}};
+}
+
+struct AST *unop(enum unop_type type, struct AST *operand) {
+    return newnode(_unop(type, operand));
+}
+
+struct AST _binop(enum binop_type type, struct AST *left, struct AST *right) {
+    return (struct AST) {AST_BINOP, .as_binop = {type, left, right}};
+}
+
+struct AST *binop(enum binop_type type, struct AST *left, struct AST *right) {
+    return newnode(_binop(type, left, right));
+}
+
+static const char *BINOPS[] = {
+        [BIN_PLUS] = "+", [BIN_MINUS] = "-", [BIN_MUL] = "*", [BIN_DIV] = "/"};
+static const char *UNOPS[] = {[UN_NEG] = "-"};
+
+typedef void(printer)(FILE *, struct AST *);
+
+
+static void print_binop(FILE *f, struct AST *ast) {
+    fprintf(f, "(");
+    print_ast(f, ast->as_binop.left);
+    fprintf(f, ")");
+    fprintf(f, "%s", BINOPS[ast->as_binop.type]);
+    fprintf(f, "(");
+    print_ast(f, ast->as_binop.right);
+    fprintf(f, ")");
+}
+
+static void print_unop(FILE *f, struct AST *ast) {
+    fprintf(f, "%s(", UNOPS[ast->as_unop.type]);
+    print_ast(f, ast->as_unop.operand);
+    fprintf(f, ")");
+}
+
+static void print_lit(FILE *f, struct AST *ast) {
+    fprintf(f, "%" PRId64, ast->as_literal.value);
+}
+
+static printer *ast_printers[] = {
+        [AST_BINOP] = print_binop, [AST_UNOP] = print_unop, [AST_LIT] = print_lit};
+
+void print_ast(FILE *f, struct AST *ast) {
+    if (ast)
+        ast_printers[ast->type](f, ast);
+    else
+        fprintf(f, "<NULL>");
+}
+
+typedef int64_t (parser)(struct AST *);
+
+// UNOP TYPE
+
+static int64_t parse_neg(struct AST *ast) {
+    return -calc_ast(ast);
+}
+
+static parser *unop_parsers[] = {
+        [UN_NEG] = parse_neg
+};
+
+// BINOP TYPE
+
+typedef int64_t (binop_parser)(struct AST *, struct AST *);
+
+#define DEFINE_SIMPLE_BINOP_PARSER(operation, operat) \
+static int64_t parse_binop_##operation(struct AST *left, struct AST *right) { \
+    return calc_ast(left) operat calc_ast(right); \
+}
+
+DEFINE_SIMPLE_BINOP_PARSER(add, +)
+
+DEFINE_SIMPLE_BINOP_PARSER(sub, -)
+
+DEFINE_SIMPLE_BINOP_PARSER(mul, *)
+
+DEFINE_SIMPLE_BINOP_PARSER(div, /)
+
+static binop_parser *binop_parsers[] = {
+        [BIN_PLUS] = parse_binop_add, [BIN_MINUS] = parse_binop_sub, [BIN_DIV] = parse_binop_div, [BIN_MUL] = parse_binop_mul
+};
+
+// AST TYPE
+
+static int64_t parse_lit(struct AST *ast) {
+    return ast->as_literal.value;
+}
+
+static int64_t parse_unop(struct AST *ast) {
+    return unop_parsers[ast->as_unop.type](ast->as_unop.operand);
+}
+
+static int64_t parse_binop(struct AST *ast) {
+    return binop_parsers[ast->as_binop.type](ast->as_binop.left, ast->as_binop.right);
+}
+
+static parser *ast_parsers[] = {
+        [AST_LIT] = parse_lit, [AST_UNOP] = parse_unop, [AST_BINOP] = parse_binop
+};
+
+
+int64_t calc_ast(struct AST *ast) {
+    if (ast)
+        ast_parsers[ast->type](ast);
+    else
+        return 0;
+}
+
+
+static void p_print_binop(FILE *f, struct AST *ast) {
+    p_print_ast(f, ast->as_binop.left);
+    p_print_ast(f, ast->as_binop.right);
+    fprintf(f, "%s ", BINOPS[ast->as_binop.type]);
+}
+
+static void p_print_unop(FILE *f, struct AST *ast) {
+    p_print_ast(f, ast->as_unop.operand);
+    fprintf(f, "%s ", UNOPS[ast->as_unop.type]);
+}
+
+static void p_print_lit(FILE *f, struct AST *ast) {
+    fprintf(f, "%"PRId64" ", ast->as_literal.value);
+}
+
+
+static printer *ast_p_printers[] = {
+        [AST_BINOP] = p_print_binop, [AST_UNOP] = p_print_unop, [AST_LIT] = p_print_lit};
+
+void p_print_ast(FILE *f, struct AST *ast) {
+    if (ast)
+        ast_p_printers[ast->type](f, ast);
+    else
+        fprintf(f, "<NULL>");
+}
---
+++ main.c
@@ -0,0 +1,137 @@
+/* main.c */
+
+#include <string.h>
+
+#include "ast.h"
+#include "ring.h"
+#include "tokenizer.h"
+
+void ast_print(struct AST ast) {
+    print_ast(stdout, &ast);
+}
+
+void token_print(struct token token) { printf("%s(%" PRId64 ")", TOKENS_STR[token.type], token.value); }
+
+DECLARE_RING(ast, struct AST)
+
+DEFINE_RING(ast, struct AST)
+
+DEFINE_RING_PRINT(ast, ast_print)
+
+DEFINE_RING(token, struct token)
+
+DEFINE_RING_PRINT(token, token_print)
+
+#define RETURN_ERROR(code, msg) return printf(msg), code
+
+const short PRECEDENCES[] = {
+        [TOK_MUL] = 2,
+        [TOK_DIV] = 2,
+        [TOK_MINUS] = 1,
+        [TOK_PLUS] = 1,
+        [TOK_NEG] = 3
+};
+
+typedef struct AST *(binop_builder)(struct AST *left, struct AST *right);
+
+static binop_builder *binop_builders[] = {
+        [TOK_MUL] = mul,
+        [TOK_DIV] = divide,
+        [TOK_MINUS] = sub,
+        [TOK_PLUS] = add
+};
+
+typedef struct AST *(unop_builder)(struct AST* node);
+
+static unop_builder *unop_builders[] = {
+        [TOK_NEG] = neg
+};
+
+struct AST build_binop(struct ring_ast **ast_build, struct ring_token **ops) {
+    return *binop_builders[ring_token_pop(ops).type]
+            (newnode(ring_ast_pop(ast_build)),
+             newnode(ring_ast_pop(ast_build)));
+}
+
+struct AST build_unop(struct ring_ast **ast_build, struct ring_token **ops) {
+    return *unop_builders[ring_token_pop(ops).type]
+            (newnode(ring_ast_pop(ast_build)));
+
+}
+
+struct AST build_op(struct ring_ast **ast_build, struct ring_token **ops, struct token operator) {
+    if(is_binop(operator)) {
+        return build_binop(ast_build, ops);
+    } else if(is_unop(operator)) {
+        return build_unop(ast_build, ops);
+    }
+}
+
+struct AST *build_ast(char *str) {
+    struct ring_token *tokens = NULL;
+    if ((tokens = tokenize(str)) == NULL)
+        RETURN_ERROR(NULL, "Tokenization error.\n");
+
+    struct ring_ast *ast_build = NULL;
+    struct ring_token *ops = NULL;
+    while (tokens != NULL) {
+        struct token tok = ring_token_pop_top(&tokens);
+        if (tok.type == TOK_LIT) {
+            ring_push_create(ast, ast_build, *lit(tok.value))
+        } else if (is_binop(tok) || is_unop(tok)) {
+            while ((ops != NULL)) {
+                struct token operator = ring_token_last(ops);
+                if(PRECEDENCES[operator.type] < PRECEDENCES[tok.type])
+                    break;
+                if(is_binop(operator) || is_unop(operator)) {
+                    ring_push_create(ast, ast_build, build_op(&ast_build, &ops, operator));
+                } else {
+                    break;
+                }
+            }
+            ring_push_create(token, ops, tok);
+        } else if (tok.type == TOK_OPEN) {
+            ring_push_create(token, ops, tok);
+        } else if (tok.type == TOK_CLOSE) {
+            while ((ops != NULL) && ring_token_last(ops).type != TOK_OPEN) {
+                ring_push_create(ast, ast_build, build_op(&ast_build, &ops, ring_token_last(ops)));
+            }
+            ring_token_pop(&ops);
+        }
+    }
+    while (ops != NULL) {
+        ring_push_create(ast, ast_build, build_op(&ast_build, &ops, ring_token_last(ops)));
+    }
+
+    struct AST *result = newnode(ring_ast_pop(&ast_build));
+    ring_token_free(&tokens);
+
+
+    return result;
+}
+
+
+int main() {
+    //char *str = "(1+ -2 )";
+    const int MAX_LEN = 1024;
+    char str[MAX_LEN];
+    if (fgets(str, MAX_LEN, stdin) == NULL)
+        RETURN_ERROR(0, "Input is empty.");
+    if (str[strlen(str) - 1] == '\n')
+        str[strlen(str) - 1] = '\0';
+
+    struct AST *ast = build_ast(str);
+
+    if (ast == NULL)
+        printf("AST build error.\n");
+    else {
+        printf("AST: \n");
+        ast_print(*ast);
+        printf("\nInfix notation: \n%s = %" PRId64 "\n", str, calc_ast(ast));
+        printf("Reverse polish notation: \n");
+        p_print_ast(stdout, ast);
+        printf(" = %" PRId64 "\n", calc_ast(ast));
+    }
+
+    return 0;
+}
